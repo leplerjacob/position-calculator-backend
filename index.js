a57d31166cbb2af3705958028fe5b5ed48ebc450
Jacob Lepler <lepler.jacob@gmail.com>
2021-09-06 18:29:54 -0400

    feat: add cors and update request handling

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const express = require('express')
-const request = require('./request.js')
-const app = express()
+const express = require("express");
+const request = require("./request.js");
+const cors = require("cors");
+const app = express();
 
-const port = 3000
+const port = 3000;
 
-app.get('/btcusd', async (req, res) => {
-    const data = await request.getBTC()
-    const json = JSON.stringify(data)
+app.use(cors());
 
-    res.send(json)
-})
+const corsOptions = {
+  origin: "http://localhost:3002",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.get("/btcusd", cors(corsOptions), async (req, res) => {
+  const data = await request.getBTC();
+  const json = JSON.stringify(data);
+
+  res.send(json);
+});
 
 // app.get('/ethusd', (req, res) => {
 //   res.send('Eth Price')
 // })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
